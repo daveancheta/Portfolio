6c798c13c7ddb52cd5f0295477560fde4273b0d6
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-01-15 00:13:47 +0800

    feat: enhance ProjectSection with construction indicator and updated layout styles

--- src/components/common/project-section.tsx
+++ src/components/common/project-section.tsx
@@ -1,13 +1,14 @@
 import { Highlighter } from "@/components/ui/highlighter"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
-import { Link2 } from "lucide-react"
+import { Construction, Link2 } from "lucide-react"
 import { HeroVideoDialog } from "@/components/ui/hero-video-dialog"
 
 function ProjectSection() {
   return ( 
-    <section className="wrapper mt-20">
-        Project Section
+    <section className="wrapper mt-20 text-2xl w-screen flex justify-center items-center flex-col">
+        <h1>Project Section</h1>
+        <span className="flex flex-row gap-1"><Construction className="animate-bounce size-10"/> under construction</span>
     </section>
   )
 }
